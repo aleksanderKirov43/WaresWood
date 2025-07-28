401785993ee738a1d0eb3597e0a998eeb111ee04
aleksanderKirov43 <region-manager43@yandex.ru>
2025-07-28 23:17:38 +0300

    add adaptive

--- front/modal_form.php
+++ front/modal_form.php
@@ -3,7 +3,7 @@
   <div class="modal-content">
     <span id="modalClose" class="modal-close">&times;</span>
     <h2>Оставьте заявку</h2>
-    <form id="modalContactForm" class="contact-form" method="post" action="send_order.php">
+    <form id="modalContactForm" class="contact-form" method="post" action="send.php">
       <input type="hidden" id="productInput" name="title" value="" />
       <input type="text" name="name" placeholder="Имя" required/>
       <input type="tel" name="phone" placeholder="Телефон*" />
