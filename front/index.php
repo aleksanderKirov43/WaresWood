bfe8e0aaf84112316918c78e90f2a321935184b9
aleksanderKirov43 <region-manager43@yandex.ru>
2025-07-29 16:07:05 +0300

    add adaptive

--- front/index.php
+++ front/index.php
@@ -3,59 +3,8 @@
 include 'products.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-
-  <title>Wares & Wood</title>
-
-  <link rel="stylesheet" href="/front/css/style.css"/>
-  <link rel="stylesheet" href="/front/css/slider.css"/>
-
-
-</head>
-<script src="/front/js/slider.js"></script>
-<body>
-
-<header>
-  <div class="container">
-    <!-- Десктоп: меню -->
-    <div class="desktop-bar">
-      <nav class="header_nav" id="nav-desktop">
-        <ul>
-          <li><a href="#catalog">Каталог</a></li>
-          <li><a href="#map">Где купить</a></li>
-          <li><a href="#delivery">Доставка</a></li>
-          <li><a href="#contact">Контакты</a></li>
-        </ul>
-      </nav>
-      <p class="phone" ><a href="tel:+79226677059">+7 (922) 667‑70‑59</a></p>
-    </div>
-
-    <!-- Мобилка: бургер -->
-    <div class="mobile-bar">
-      <div class="burger" id="burger">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <p class="phone"><a href="tel:+79226677059">+7 (922) 667‑70‑59</a></p>
-    </div>
-
-    <!-- Мобильное меню -->
-    <nav class="header_nav" id="nav-mobile">
-      <ul>
-        <li><a href="#catalog">Каталог</a></li>
-        <li><a href="#map">Где купить</a></li>
-        <li><a href="#delivery">Доставка</a></li>
-        <li><a href="#contact">Контакты</a></li>
-      </ul>
-    </nav>
-
-  </div>
-</header>
+<?php include 'header.php'
+?>
 
 
 <main class="utp-block">
