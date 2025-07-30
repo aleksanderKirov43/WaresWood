f1003cd8df312164a9d8fd1ff986e9f720cde6fc
aleksanderKirov43 <region-manager43@yandex.ru>
2025-07-30 21:47:00 +0300

    add adaptive

--- front/footer.php
+++ front/footer.php
@@ -32,6 +32,6 @@
   <script src="/front/js/telegram.js"></script>
   <script src="/front/js/notifications.js"></script>
   <script src="/front/js/form-photo.js"></script>
-  <script src="/front/js/product-slider.js"></script>
+  <!-- <script src="/front/js/product-slider.js"></script> -->
   <!-- <script src="/front/js/main.js"></script> -->
   <!-- <script src="/front/js/slider.js"></script> -->
