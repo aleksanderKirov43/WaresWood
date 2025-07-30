d3538825c9eceafabd224b5bce8102cb9c81ef11
aleksanderKirov43 <region-manager43@yandex.ru>
2025-07-30 21:33:24 +0300

    add adaptive

--- front/footer.php
+++ front/footer.php
@@ -30,15 +30,9 @@
   window.productImages = <?= json_encode($product['images']) ?>;
   </script>
 
-  <script src="/front/js/telegram.js"></script>
-  <script src="/front/js/notifications.js"></script>
-  <script src="/front/js/form-photo.js"></script>
-  <script src="/front/js/product-slider.js"></script>
-  <script src="/front/js/main.js"></script>
-  <script src="/front/js/slider.js"></script>
-  <script>
-  console.log('JS базовый тест');
-  document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM загружен');
-  });
-</script>
+<script src="/front/js/telegram.js"></script>
+<script src="/front/js/notifications.js"></script>
+<script src="/front/js/form-photo.js"></script>
+<script src="/front/js/product-slider.js"></script>
+<script src="/front/js/main.js"></script>
+<script src="/front/js/slider.js"></script>
