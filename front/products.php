0dc13efaaa4cd2401d29f75d8e21413d2d3242f3
aleksanderKirov43 <region-manager43@yandex.ru>
2025-07-30 11:59:30 +0300

    add adaptive

--- front/products.php
+++ front/products.php
@@ -5,21 +5,21 @@ $products = [
         'title' => 'Стеллаж 182х39х32 см',
         'description' => 'Размеры:
         Высота: 1820 мм. Ширина: 390 мм. Глубина: 320 мм.',
-        'images' => ['/front/images/product-s1.jpg', '/front/images/product-s1.jpg'],
+        'images' => ['/images/product-s1.jpg', '/images/product-s1.jpg'],
         'price' => '1500 руб.'
     ],
     'shelf1' => [
         'title' => 'Стеллаж 182х39х32 см',
         'description' => 'Размеры:
         Высота: 1820 мм. Ширина: 760 мм. Глубина: 320 мм.',
-        'images' => ['/front/images/product-s2.jpg', '/front/images/product-s2.jpg'],
+        'images' => ['/images/product-s2.jpg', '/images/product-s2.jpg'],
         'price' => '1820 руб.'
     ],
     'shelf2' => [
         'title' => 'Стеллаж 182х113х32 см',
         'description' => 'Размеры:
         Высота: 1820 мм. Ширина: 1130 мм. Глубина: 320 мм.',
-        'images' => ['/front/images/product-s3.jpg', '/front/images/product-s3.jpg'],
+        'images' => ['/images/product-s3.jpg', '/images/product-s3.jpg'],
         'price' => '3500 руб.'
     ],
 ];
